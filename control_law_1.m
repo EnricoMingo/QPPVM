cde6bdbb3199399d5e3a27d25511b4d029b020cd
Enrico Mingo <enrico.mingo@iit.it>
2016-09-21 18:48:21 +0200

    Added some coments on the code

--- control_law_1.m
+++ control_law_1.m
@@ -1,36 +1,55 @@
 function tau = control_law_1(robot, t, q, qdot)
+%% control_law_1 
+% Uses a QP to solve the problem to find joint torques that realizes a
+% certain Cartesian Force at the end-effector considering also low priority
+% tasks and inequality constraints.
+%
+% Note that the quadratic cost function is:
+%     F = ||(pinv_J_B)'*tau - f||
+% with pinv_J1_B the dynamically consistent pseudo-inverse of J,
+% and the Optimality conditions for low priority tasks is:
+%     (pinv_J_B)'*tau = (pinv_J_B)'*tau_
+% where tau_ = argmin F of the previous task.
 
 t
 
+%% Actual information from the robot
+Tq = robot.fkine(q); % pose
+J = robot.jacob0(q); % Jacobian
+B = robot.inertia(q); % Inertia
+Binv = inv(B); 
+taumax = [1 1 1 1 1 1]'*100; % Max allowed torques
+taumin = -taumax; % Min allowed torques
+
+%% Reference Trajectory for Main Task (the main task is a Cartesian Position Trj Task)
 period = 2;
 amplitude = 0.3;
+x1ref = amplitude*sin(2*pi/period*t);
+
 Kp1 = 15000;
 Kd1 = 300;
-Kp2 = 15000;
-Kd2 = 400;
-Tq = robot.fkine(q);
-J = robot.jacob0(q);
-B = robot.inertia(q);
-Binv = inv(B);
-taumax = [1 1 1 1 1 1]'*100;
-taumin = -taumax;
-opt = optimoptions('quadprog', 'Display', 'off');
 
-x1ref = amplitude*sin(2*pi/period*t);
 x1 = Tq(3,4);
 J1 = J(3,:);
-B1 = inv(J1*Binv*J1');
-J1pinv = Binv*J1'*B1;
 x1dot = J1*qdot';
 f1 = -Kp1*(x1-x1ref) - Kd1*x1dot;
+
+%% Reference for Secondary task (the secondary task is a Cartesian Position Task)
+Kp2 = 15000;
+Kd2 = 400;
 
 x2ref = [0.2 0.2]';
 x2 = Tq(1:2,4);
 J2 = J(1:2,:);
-B2 = inv(J2*Binv*J2');
-J2pinv = Binv*J2'*B2;
 x2dot = J2*qdot';
 f2 = -Kp2*(x2-x2ref) - Kd2*x2dot;
+
+%% Solution using QP
+opt = optimoptions('quadprog', 'Display', 'off', 'Algorithm', 'active-set');
+
+% Solution of Main Task
+B1 = inv(J1*Binv*J1');
+J1pinv = Binv*J1'*B1;
 
 Q1 = J1pinv*J1pinv';
 c1 = -f1'*J1pinv';
@@ -38,20 +57,23 @@ A1 = [];
 b1 = [];
 tau1 = quadprog(Q1,c1,[],[],A1,b1,taumin, taumax,[],opt);
 
+% Solution of the Secondary Task
+B2 = inv(J2*Binv*J2');
+J2pinv = Binv*J2'*B2;
 
 Q2 = J2pinv*J2pinv';
 c2 = -f2'*J2pinv';
-A2 = J1pinv';
-b2 = J1pinv'*tau1;
+A2 = J1pinv'; % Optimality condition
+b2 = J1pinv'*tau1; % Optimality condition
 tau1 = quadprog(Q2,c2,[],[],A2,b2,taumin, taumax,[], opt);
-f2_opt = J2pinv'*tau1;
+%f2_opt = J2pinv'*tau1;
 
+% Solution of a Third Task in Joint space (Joint Torque minimzation)
 Q3 = eye(6);
 c3 = zeros(6,1);
-A3 = [J1pinv'; J2pinv'];
-b3 = A3*tau1;
+A3 = [J1pinv'; J2pinv']; %Optimality Condition
+b3 = A3*tau1; %Optimality Condition
 tau1 = quadprog(Q3,c3,[],[],A3,b3, taumin, taumax, [], opt);
-
 
 tau = tau1';
 
