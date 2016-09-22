a5b61637b0ba8c340bfd4c874f243cfddf7f781f
Arturo Laurenzi <arturo.laurenzi@iit.it>
2016-09-22 19:41:13 +0200

    Add handle class for data exchange with controllers

---
+++ ControllerIO.m
@@ -0,0 +1,15 @@
+classdef ControllerIO < handle
+    %CONTROLLERIO Class to handle input/output to/from controllers
+    %   Just put everything you want inside the struct Data
+    
+    properties
+        
+        Data = struct();
+        
+    end
+    
+    methods
+    end
+    
+end
+
